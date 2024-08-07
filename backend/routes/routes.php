5a2b935605ffe60398f669df868035df06f97a5c
stNeweR <shyguy096com@gmail.com>
2024-08-07 10:33:42 +0700

    Добавил xdebug в docker

--- backend/routes/routes.php
+++ backend/routes/routes.php
@@ -3,10 +3,15 @@
 use App\Http\Controllers\UserController;
 use Kernel\Router\Route;
 
+$a = 10;
 
+var_dump($a);
+
+var_dump(++$a);
+xdebug_info();
+phpinfo();
 Route::get('', [UserController::class, 'slash'])->name('test1');
 Route::get('/new', [UserController::class, 'index'])->name('test1');
-// Route::get('/new/{new}', [UserController::class, 'show'])->name('test2');
 Route::post('/', [UserController::class, 'createNew'])->name('test1');
 Route::post('/new', [UserController::class, 'create'])->name('test2');
 Route::put('/', [UserController::class, 'update'])->name('test1');
